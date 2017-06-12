64193a71f1ca061bad7370db5649dd1584000581
amit4alljava <amit4alljava@gmail.com>
2017-06-12 17:36:00 +0530

    Codes

---
+++ Calc/js/controller.js
@@ -0,0 +1,38 @@
+/*
+Glue B/w View and Model
+1. Get All Operation Buttons
+*/
+window.addEventListener("load",bindEvents);
+function bindEvents(){
+	var btArray = document.getElementsByClassName("operation");
+	for(var i = 0; i<btArray.length; i++){
+		btArray[i].addEventListener("click",doOperation);
+	}
+}
+function doOperation(){
+	//var opr = this.getAttribute("data-opr");
+	//console.log("Opr is ",opr);
+	var opr = this.innerHTML;
+	var firstNo = document.getElementById("firstno").value;
+	var secondNo = document.getElementById("secondno").value;
+	var result= 0;
+	result= commonOperation(firstNo,secondNo,opr);
+	//result = calcObject[opr](firstNo,secondNo);
+//	if(opr=='+'){
+//		result = add(firstNo,secondNo);
+//	}
+//	else
+//	if(opr=='-'){
+//		result = subtract(firstNo,secondNo);
+//	}
+//	else
+//	if(opr=='*'){
+//		result = multiply(firstNo,secondNo);
+//	}
+//	else
+//	if(opr=='/'){
+//		result = divide(firstNo,secondNo);
+//	}
+	document.getElementById("result").innerHTML = result;
+}
+
